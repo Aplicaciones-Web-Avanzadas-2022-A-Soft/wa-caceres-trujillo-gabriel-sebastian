9ae3b399af44c7b58a584f20d43ae63faabbe9ed
CaceresG <gabriel.caceres@epn.edu.ec>
2022-05-18 08:57:48 -0500

    Actualizacion de operadores y creacion de funciones

---
+++ 01-javascript/04-funciones.js
@@ -0,0 +1,15 @@
+function soloNumeros(a,b,c){
+    return a - b + c;
+}
+
+console.log(soloNumeros(1,2,3,true,[1,2,3]))
+
+function funcionNombrada(){
+
+}
+
+const funcionSinNombre=function (){};
+
+funcionSinNombre();
+
+const funcionFatArrow= () => {}
