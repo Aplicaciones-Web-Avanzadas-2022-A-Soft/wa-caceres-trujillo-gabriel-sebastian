152e389d15741c50b4d3e2434b033fc285a5c85d
CaceresG <gabriel.caceres@epn.edu.ec>
2022-06-07 08:59:44 -0500

    Creación de la primera página

--- 03-react/react-nextjs/pages/about.tsx
+++ 03-react/react-nextjs/pages/about.tsx
@@ -5,6 +5,7 @@ const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
     <p>This is the about page</p>
+    <p>Hola Mundo 25</p>
     <p>
       <Link href="/">
         <a>Go home</a>
---
+++ 03-react/react-nextjs/pages/mi-perfil.tsx
@@ -0,0 +1,29 @@
+import Layout from "../components/Layout";
+
+const MiPerfilPage = () =>{
+
+    const arreglo= [1,2,3,4,5,6,7,8];
+
+    const listaNumeros = arreglo
+        .filter((a)=>a<5)
+        .map((a)=>{
+            return(
+                <li>
+                    {a}
+                </li>
+            )
+        })
+
+    return (
+        <Layout>
+            <p>hello {"hola".toUpperCase()}</p>
+            Que tal
+            <ul>
+                {listaNumeros}
+            </ul>
+        </Layout>
+    )
+    
+}
+
+export default MiPerfilPage
